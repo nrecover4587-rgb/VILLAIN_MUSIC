3de0563e5d244c87a434b02d6205508c7edf7f09
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-17 15:10:24 +0530

    Update button text and URL in couples.py

--- VILLAIN_MUSIC/plugins/tools/couples.py
+++ VILLAIN_MUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from VILLAIN_MUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="ᯓ𝐈тᷟʑ‌꯭ 🔰 𝚸‌ 𝚵 །⃪᱂ ƒ‌ 𝚵 𝐂 𝛕 ᴵᴰ'ˢ",
+            url=f"https://t.me/PerfectseIler",
         ),
     ],
 ]
