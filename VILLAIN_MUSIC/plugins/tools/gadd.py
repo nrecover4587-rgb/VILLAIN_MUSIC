ce95c60d8842f9d0e066af78a11e05e238fe9204
nrecover4587-rgb <nrecover4587@gmail.com>
2026-02-27 11:38:46 +0530

    Update gadd.py

--- VILLAIN_MUSIC/plugins/tools/gadd.py
+++ VILLAIN_MUSIC/plugins/tools/gadd.py
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002011723196:
+            if dialog.chat.id == -1003670305112:
                 continue
             try:
 
