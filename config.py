108c6e918d1acbcc3bc3fdd20bbcf6eb0f17107e
nrecover4587-rgb <nrecover4587@gmail.com>
2026-02-27 11:25:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","Thakurrr_366")
+OWNER_USERNAME = getenv("OWNER_USERNAME","MrMental001")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","Sarkar144_bot")
+BOT_USERNAME = getenv("BOT_USERNAME","Romanticxmusicbot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
@@ -27,10 +27,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID",-1003000277703 ))
+LOGGER_ID = int(getenv("LOGGER_ID",-1003670305112 ))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID",7018521436 ))
+OWNER_ID = int(getenv("OWNER_ID",7113972959 ))
 
 
 # make your bots privacy from telegra.ph and put your url here 
@@ -51,8 +51,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BOTxBOOSTER")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BOTxBOOSTER")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/codexempire")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mentalchatting")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
